5fcb9d3da37009da04c74a0df7d9facad3fe9fa4
win11 <email@email.com>
2024-02-29 20:58:39 -0700

    2nd update

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -11,14 +11,12 @@ const Cta = () => {
 
     return (
         <div className="w-full flex items-center justify-center text-blue-900" style={sectionStyle}>
-            <div className="container mx-auto p-12 text-center lg:text-left flex flex-col lg:flex-row items-center justify-between bg-gray-100 bg-opacity-80 rounded-lg">
-                <div className="lg:flex-grow mb-6 lg:mb-0">
+            <div className="container mx-auto p-12 text-center lg:text-left flex flex-col  items-center justify-between bg-gray-100 bg-opacity-80 rounded-lg">
+                <div className="flex flex-col justify-center text-center">
                     <h2 className='text-3xl md:text-5xl font-bold mb-4'>Contact Us</h2>
                     <p className="text-lg md:text-xl">Have a question or need a quote? Reach out to us and let's discuss how we can help with your gabion needs.</p>
                     <p className="mt-4">Email: <a href="mailto:contact.aggie@pm.me" className="text-blue-900 underline">contact.aggie@pm.me</a></p>
                     <p className="mt-4">Phone: <a href="tel:+14039690333" className="text-blue-900 underline">(403)-969-0333</a></p>
-                </div>
-                <div className="lg:flex-grow">
                 </div>
             </div>
         </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Intro = () => {
     return (
         <>
-            <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id='about'>
+            <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-5/6" id='about'>
                 <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
-                    <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+                    <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center text-center">
                         <img alt="Gabion baskets" className="rounded-t float-right duration-500 w-full" src="/images/gabion-garden.jpeg" />
                     </div>
-                    <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
+                    <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="500">
                         <h3 className="text-3xl text-blue-900 font-bold">Discover the Durability and Versatility of Our Gabion Baskets, Panels, and Fences</h3>
                         <div>
                             <p className='my-3 text-xl text-gray-600 font-semibold'>Our gabion products are engineered for stability and designed to enhance the beauty of your outdoor space.</p>
