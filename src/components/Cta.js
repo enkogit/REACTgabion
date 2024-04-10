774b82cce829edfd53fc9747a1ee1adf7951ed3c
Viktor Nowiczenko <viktor.nowiczenko@insight-awp.com>
2024-04-10 10:25:32 -0600

    update contact email

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -15,7 +15,7 @@ const Cta = () => {
                 <div className="flex flex-col justify-center text-center">
                     <h2 className='text-3xl md:text-5xl font-bold mb-4'>Contact Us</h2>
                     <p className="text-lg md:text-xl">Have a question or need a quote? Reach out to us and let's discuss how we can help with your gabion needs.</p>
-                    <p className="mt-4">Email: <a href="mailto:contact.aggie@pm.me" className="text-blue-900 underline">contact.aggie@pm.me</a></p>
+                    <p className="mt-4">Email: <a href="mailto:contact.aggie@pm.me" className="text-blue-900 underline">gabionca@pm.me</a></p>
                     <p className="mt-4">Phone: <a href="tel:+14039690333" className="text-blue-900 underline">(403)-969-0333</a></p>
                 </div>
             </div>
